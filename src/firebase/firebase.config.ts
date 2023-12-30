2e576851d52a9abfad04d401f47edb39a57a40a3
taiyebnirjhar <nirjhar.212902003@green.ac.bd>
2023-12-30 06:08:01 +0600

    update

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQxXRYhm7yeYj3LUXjR1tFQ5k5-nZGsao",
+  authDomain: "tryingtoimpressfringcore.firebaseapp.com",
+  projectId: "tryingtoimpressfringcore",
+  storageBucket: "tryingtoimpressfringcore.appspot.com",
+  messagingSenderId: "888679086376",
+  appId: "1:888679086376:web:e2d896c4a78e0602a1f81d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <div> I AM HERE</div>,
+        element: <QuizForJs />,
       },
       {
         path: "/dive-into-javascript",
